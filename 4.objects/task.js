504905c22fd246d4aa8be1dc574ebb4bb2dc07af
Vitaly Kovalenko <rtm_wrk@rambler.ru>
2023-07-30 00:29:47 +0300

    Initial app -> homework completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,48 @@
+"use strict";
+
+// --- Функция - конструктор объекта "студент"" ---
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+	return this; 
 }
 
+// --- Метод, добавляющий учебный предмет ---
 Student.prototype.setSubject = function (subjectName) {
-  
+  	this.subject = subjectName;
 }
 
+// --- Метод, добавляющий оценки ---
 Student.prototype.addMarks = function (...marks) {
-  
+	if (this.hasOwnProperty("marks")) {
+	 	this.marks.push(...marks);
+	}	
 }
 
-Student.prototype.getAverage = function () {
-  
+// --- Методы расчета средней оценки ---
+Student.prototype.getAverage = function () {	
+	if (this.hasOwnProperty("marks")) {
+		return this.marks.reduce((value, item, index, arr) => {
+	  		if (arr.length === 0) { 
+	  			return 0; 
+	  		} else {
+				if (index === arr.length - 1) { 
+					return (value += item) / arr.length; 
+				} else { 
+					return value += item; 
+				}
+			}		
+		}, 0);
+	} else {
+		return 0;
+	}
 }
 
+// --- Метод формированния объекта "отчисленный студент" ---
 Student.prototype.exclude = function (reason) {
-  
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;  
 }
